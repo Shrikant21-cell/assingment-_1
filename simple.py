3109e3ffb67c9772946c7c007fb6456361339bdc
Shrikant21-cell <shrikant.parshivkar2005@gmail.com>
2025-07-16 22:36:30 +0530

    Add files via upload

---
+++ simple.py
@@ -0,0 +1,19 @@
+#task 1
+a=input('enter the first number')
+b=input('enter the second number')
+a=int(a)
+b=int(b)
+p=a+b
+print('Addition:',p)
+o=a-b
+print('substraction:',o)
+i=a*b
+print('multiplication:',i)
+u=a/b
+print('division:',u)
+#task 2
+x=input('enter your first name:')
+y=input('enter yuor last name:')
+x=str(x)
+y=str(y)
+print(' Hello,\t'+x,y +' ! welcome to the python program')
